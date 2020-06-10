1c81c84d2a9186d28f164296a8636baed625593e
SarahNaik1 <sarahnaik2@gmail.com>
2020-06-10 11:42:17 +0530

    Added data cleaning step

--- algorithm/randomForestAlgorithm/randomForestClassification.py
+++ algorithm/randomForestAlgorithm/randomForestClassification.py
@@ -13,6 +13,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+from sklearn.model_selection import RandomizedSearchCV
 import seaborn as sn
 import matplotlib.pyplot as plt
 
@@ -56,3 +57,34 @@ df_cm = pd.DataFrame(results, index = [i for i in ["Aggressive Investor", "Conse
                   columns = [i for i in ["Aggressive Investor", "Conservative Investor", "Moderate Investor", "Moderately Aggressive Investor", "Moderately Aggressive Investor"]])
 plt.figure(figsize = (10,8))
 sn.heatmap(df_cm, annot=True)
+
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4]
+# Method of selecting samples for training each tree
+bootstrap = [True, False]
+# Create the random grid
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
+print(random_grid)
+
+# Use the random grid to search for best hyperparameters
+# First create the base model to tune
+rf = RandomForestClassifier()
+# Random search of parameters, using 3 fold cross validation,
+# search across 100 different combinations, and use all available cores
+rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, cv = 3, verbose=2, random_state=42)
+# Fit the random search model
+rf_random.fit(train_features, train_labels)
